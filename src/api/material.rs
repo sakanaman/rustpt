70a8232c20ac43f9aa4a5f12cfb4c2e5e1e8024f
sakanaman <sakana1231@icloud.com>
2024-04-10 20:28:57 +0900

    fix: reference

--- src/api/material.rs
+++ src/api/material.rs
@@ -21,9 +21,9 @@ impl SampleBSDF for Diffuse {
         let r1 =  2.0 * PI * u;
         let r2s = f64::sqrt(r2);
         let tans = normal.onb(); 
-        let nextvec =    &tans.0 * f64::cos(r1) * r2s 
-                                + &tans.1 * f64::sin(r1) * r2s 
-                                +  normal * f64::sqrt(1.0 - r2) ;
+        let nextvec =    tans.0 * f64::cos(r1) * r2s 
+                                + tans.1 * f64::sin(r1) * r2s 
+                                + normal * f64::sqrt(1.0 - r2) ;
         (self.albedo.clone(), Ray{org: ray.org.clone(), dir: nextvec})
     }
     fn pdf(&self, _ray : &Ray, normal : &Vector3, next_ray : &Ray)->f64 {
