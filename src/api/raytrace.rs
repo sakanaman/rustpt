158e759f341c38560caa4052584f6578c1a71a02
sakanaman <sakana1231@icloud.com>
2024-05-26 19:49:47 +0900

    add russian-roulette, light

--- src/api/raytrace.rs
+++ src/api/raytrace.rs
@@ -7,6 +7,9 @@ use crate::api::figure::Figure;
 use crate::api::search::LinearSearch;
 use crate::api::search::SearchAlgorithm;
 use rand::rngs::ThreadRng;
+use rand::Rng;
+
+const RUSSIAN_PROB : f64 = 0.9;
 
 pub fn ray_trace(first_ray : Ray, searcher : &LinearSearch<Figure>, rng : &mut ThreadRng) -> Vector3{
     let mut contribution = Vector3{x:1.0, y:1.0, z:1.0};
@@ -27,9 +30,16 @@ pub fn ray_trace(first_ray : Ray, searcher : &LinearSearch<Figure>, rng : &mut T
        let hitdata = wrapped_hitdata.unwrap();
 
         // BSDF(or Volume) sample
+        let mut is_light = false;
         let material_info = 
         match shape {
             Figure::Sphere(s) => {
+                if s.emission.x > 0.0 || s.emission.y > 0.0 || s.emission.z > 0.0
+                {
+                    contribution = &contribution * &s.emission;
+                    is_light = true;
+                } 
+
                 match &(s.material) {
                     Material::Diffuse(m) => {
                         m.sample(&ray, &hitdata.normal, rng)
@@ -37,7 +47,17 @@ pub fn ray_trace(first_ray : Ray, searcher : &LinearSearch<Figure>, rng : &mut T
                 }
             }
         };
-        contribution = contribution * material_info.0;
+
+        if is_light == true {
+           return contribution;
+        }
+
+        if RUSSIAN_PROB < rng.gen() {
+            contribution = Vector3{x:0.0, y:0.0, z:0.0};
+            break;
+        }
+
+        contribution = contribution * material_info.0 * (1.0 / RUSSIAN_PROB);
         ray = material_info.1;
     }
 
